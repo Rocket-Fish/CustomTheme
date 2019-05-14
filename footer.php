a35a9d3237622dc29fe4921f041872286fe59bd9
Rocket-Fish <rocket.fish@mail.com>
2019-05-14 14:52:37 -0400

    added navbar and footer

--- footer.php
+++ footer.php
@@ -10,7 +10,39 @@
 		</div> <!-- container --> 
 
 		<footer class = 'master-footer'>
-			<p> some footer content here </p> 
+			<div class="container">
+				<div class="row text-center text-xs-center text-sm-left text-md-left">
+					<div class="col-xs-12 col-sm-4 col-md-4">
+						<h5> About Clone Steele </h5> 
+						<p class = 'about'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo purus in lacus feugiat accumsan. Suspendisse nulla augue, blandit sit amet leo sollicitudin, commodo interdum ipsum. Maecenas sagittis eros eget laoreet viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque rutrum elementum tempor. Ut tincidunt vitae purus id efficitur. Donec at mattis felis. Vestibulum nunc dolor, lobortis sed accumsan sit amet, elementum id libero. </p> 
+					</div>
+					<div class="col-xs-12 col-sm-4 col-md-4">
+						<h5>Quick links</h5>
+						<ul class="list-unstyled quick-links">
+							<li><a href="#">Home</a></li>
+							<li><a href="#">About</a></li>
+							<li><a href="#">FAQ</a></li>
+							<li><a href="#">Get Started</a></li>
+						</ul>
+					</div>
+					<div class="col-xs-12 col-sm-4 col-md-4">
+						<h5>Quick links</h5>
+						<ul class="list-unstyled quick-links">
+							<li><a href="#">Home</a></li>
+							<li><a href="#">About</a></li>
+							<li><a href="#">FAQ</a></li>
+							<li><a href="#">Get Started</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+						<p><u><a href="https://deepgreenocean.com/">Deep Green Ocean</a></u> is a Registered ™ of Jimmy</p>
+						<p class="h6">&copy All right Reversed.<a class="text-green ml-2" href="https://deepgreenocean.com/" target="_blank">Jimmy</a></p>
+					</div>
+					</hr>
+				</div>	
+			</div>
 		</footer>
 
 		<?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -25,8 +25,10 @@
 			$bootstrap_js_src  = 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js';
 			$jquery_slim_src   = 'https://code.jquery.com/jquery-3.3.1.slim.min.js';
 			$poppers_min_src   = 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js';
+			$custom_style_src  = get_bloginfo( 'template_directory' ) . '/style.css';
 
 			wp_enqueue_style( 'bootstrap4-style', $bootstrap_css_src, array(), '4.3.1' );
+			wp_enqueue_style( 'custom-style', $custom_style_src, array( 'bootstrap4-style' ), null );
 			wp_enqueue_script( 'jqueryslim', $jquery_slim_src, array( 'jquery' ), '3.3.1', true );
 			wp_enqueue_script( 'poppers', $poppers_min_src, array( 'jquery' ), '3.3.1', true );
 			wp_enqueue_script( 'bootstrap4-script', $bootstrap_js_src, array( 'jquery' ), '3.3.1', true );
@@ -40,14 +42,43 @@
 	</head>
 	<body>
 
-	<div class = 'master-header-bar'> 
-		<div class = 'container'>
-			<nav class = 'navbar'> 
-				<a class = "navbar-item active" href="#"> Home </a> 
-				<a class = "navbar-item" href="#"> Other Stuff </a> 
-				<a class = "navbar-item" href="#"> Some more Other stuff </a> 
-			</nav> 
-		</div>
-	</div> <!-- master-header-bar --> 
+		<nav class = 'navbar navbar-expand-lg navbar-light bg-light'> 
+			<button class = 'navbar-toggler' type = 'button' data-toggle = 'collapse' data-target = '#navbarToggler' aria-controls = 'navbarToggler' aria-expanded = 'false' aria-label = 'Toggle Navibation'> 
+				<span class = 'navbar-toggler-icon'></span> 
+			</button> 
+			<div class = 'container'>
+				<div class = 'collapse navbar-collapse' id = 'navbarToggler'>
+					<ul class = 'navbar-nav mr-auto mt-2 mt-lg-0'>
+						<li class="nav-item active">
+							<a class="nav-link" href="#"><?php bloginfo( 'name' ); ?> <span class="sr-only">(current)</span></a>
+						</li>
+					</ul> <!-- navbar-nav mr-auto mt-2 mt-lg-0 -->
+					<ul class = 'navbar-nav my-2 my-lg-0'>
+						<?php
+						// get list of pages in the navbar.
+						$pages_list = get_pages( array( 'sort_order' => 'desc' ) );
+						// iterate through pages list for each navbar element.
+						foreach ( $pages_list as $p ) {
+							?>
+							<li class="nav-item">
+								<a class="nav-link" href="#">
+									<?php
+										$post_title = '' . $p->post_title;
+										// escape string for security.
+										echo esc_textarea( $post_title );
+									?>
+								</a>
+							</li>
+						<?php } ?>
+						<li class="nav-item">
+							<a class="nav-link disabled" href="#">About</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled" href="#">Contact</a>
+						</li>
+					</ul> <!-- navbar-nav my-2 my-lg-0 --> 
+				</div> <!-- collapse navbar-collapse --> 
+			</div> <!-- container --> 
+		</nav> <!-- 'navbar navbar-expand-lg navbar-light bg-light'--> 
 
-	<div class='container'> 
+		<div class='container'> 
